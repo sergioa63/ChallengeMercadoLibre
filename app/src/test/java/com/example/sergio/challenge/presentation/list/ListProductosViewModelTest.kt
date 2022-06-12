92e134f2f9b02082bea17ae030b9681252910023
spinto <spinto@wi-mobile.com>
2022-06-11 23:11:27 -0500

    add apk

--- app/src/test/java/com/example/sergio/challenge/presentation/list/ListProductosViewModelTest.kt
+++ app/src/test/java/com/example/sergio/challenge/presentation/list/ListProductosViewModelTest.kt
@@ -1,13 +1,28 @@
 package com.example.sergio.challenge.presentation.list
 
+import com.example.sergio.challenge.CorutineTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.*
 
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class ListProductosViewModelTest {
+
+
+    @get: Rule
+    private val coroutineTestRule = CorutineTestRule()
 
     @Before
     fun setUp() {
     }
 
+
+    @Test
+    fun `cargar lista documentos vacia` () = coroutineTestRule.testDispacher.runBlockingTest {
+
+    }
 }
